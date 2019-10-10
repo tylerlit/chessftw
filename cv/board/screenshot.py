aa5e03892f46bdadb03ed2ff412459c2f6d201e7
Tyler Little <tyler10@umbc.edu>
2019-10-10 18:39:24 -0400

    forgot to commit 1 line fml

--- cv/board/screenshot.py
+++ cv/board/screenshot.py
@@ -1,7 +1,8 @@
 import pyscreenshot as ig
 import uuid
 
-img = ig.grab()
-img.save(str(uuid.uuid4()) + ".png")
+if __name == "__main__":
+	img = ig.grab()
+	img.save(str(uuid.uuid4()) + ".png")
 
 #img.show()
