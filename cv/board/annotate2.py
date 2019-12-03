a272a83e8dd307fdd3898f3114a3bae235e6d3f1
Tyler Little <tlittle@hm1842.com>
2019-12-03 16:11:38 -0500

    get pieces meow

--- cv/board/annotate2.py
+++ cv/board/annotate2.py
@@ -2,19 +2,16 @@
 # coding: utf-8
 
 # In[1]:
-
-
 import cv2
 import draw
 import re
+import math
 import os
 import tkinter as tk
 from PIL import Image
 from matplotlib import pyplot as plt
 import numpy as np
 import tkinter as tk
-
-#
 
 # UI class for annotating pieces
 class Piece(tk.Frame):
@@ -75,58 +72,63 @@ def getBoard(image, og, cropxy, size):
     return board, where
 
 # In[2]:
-
-
 # get all files in current directory (cv\board\)
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 
 # get all screenshots from current dir
 images = [i for i in files if (i[-4:] == ".png")]
 
-# print(files)
-# print(images)
-
-
 # In[3]:
-
-
 for i in images:
 
     img = cv2.imread(i, 0)
     og = img.copy()
     size = img.shape
 
-    # get cropped image and dimensions of crop relative to original image
+    # draw cropped image and dimensions of crop relative to original image
     crop, cropxy = draw.draw(i)
 
-    # convert to grayscale
-    # theres probably a way more efficient alternative to this
-    # but hey who cares amirite
-    file = "test.jpg"
-    cv2.imwrite(file, crop)
-    img = cv2.imread(file, 0)
-
-    copy = img.copy()
+    # convert result to grayscale
+    img = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
 
     # crop out background pixels that are not the board
     # also calculate starting x,y and ending x,y of board relative to original image
     board, dimensions = getBoard(img, og, cropxy, size)
 
-    print("show test")
-    test = og[dimensions[0][1]:dimensions[1][1], dimensions[0][0]:dimensions[1][0]]
-    cv2.imshow("test", test)
-    cv2.waitKey(0)
+    # print("show test")
+    # test = og[dimensions[0][1]:dimensions[1][1], dimensions[0][0]:dimensions[1][0]]
 
-    # print(img)
-    # print(img.shape)
+    # cv2.imshow("test", test)
+    # cv2.waitKey(0)
 
     # img = cv2.resize(img, None ,fx=0.25,fy=0.25)
     # img = img[41:228, 143:329]
     
-    #print(round(img.shape[0] / 8))
-    #print(img[0,0])
+    # print(round(img.shape[0] / 8))
+    # print(img[0,0])
     
-    # unit = round(img.shape[0] / 8)
+    y = 0
+    x = 0
+    unit = math.trunc(board.shape[0] / 8)
+    middle = math.trunc(unit / 2)
+
+    for j in range(8):
+
+    	for k in range(8):
+	    	piece = board.copy()[y:unit * (j + 1), x:unit * (k + 1)]
+
+	    	if piece[0][0] == piece[middle][middle]:
+	    		continue
+	    	else:
+	    		cv2.imshow("test", piece)
+	    		cv2.waitKey(0)
+
+	    	x += unit
+
+    	x = 0
+    	y += unit
+
+
     
     # img = img[:unit, :unit]
     
