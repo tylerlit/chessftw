d32a26dd9103f6441c5d92f8adedd0b9f1e18cd1
Tyler Little <tyler10@umbc.edu>
2019-12-04 21:55:02 -0500

    added argparse to main - run using python main.py -a or -s

--- cv/board/screenshot.py
+++ cv/board/screenshot.py
@@ -1,38 +1,37 @@
 import pyscreenshot as ig
 import uuid
-import sql.mysqlutil as db
-import cv.board.helpers as helpers
+import os
+import sys
+from s3 import S3_utils
+from sql import mysqlutil as db
 
-if __name__ == "__main__":
+def run():
+	#if __name__ =='__main__':
 	#random file name
-	savePath = str(uuid.uuid4()) + ".png"
-
-	#open db connection
-	mydb = db.dbConnection()
-	mydb.openConnection()
+	fileName = str(uuid.uuid4()) + ".png"
+	savePath = "cv/board/temp/" + fileName
 
 	#get screenshopt and save temporarily
 	img = ig.grab()
 	img.save(savePath)
 
-	#read the image file as binary
-	binaryData = helpers.convertToBinaryData(savePath)
+	#store in S3
+	S3objectName = 'board/' + fileName
+	if S3_utils.upload_file(savePath,'chessftw',S3objectName) :
+		print('screenshot uploaded to S3 successfully!')
+	else:
+		sys.exit('screenshot failed to upload to S3')
+	
+	#store file path in DB
+	mydb = db.dbConnection()
+	mydb.openConnection()
 
-	#insert into the database
-	mydb.insert("""INSERT INTO chess.image
-				(id,image) VALUES (%s,%s)""", ("DEFAULT",binaryData))
+	mydb.insert("""INSERT INTO chess.board
+				(ID,Filepath) VALUES (%s,%s)""", ("DEFAULT",S3objectName))
 
-	#retrieve image from db and write to file
-	results = mydb.select("""SELECT id,image FROM chess.image
-							ORDER BY id DESC LIMIT 1""")
-
-	for row in results:
-		fileName = "TEST_" + str(row[0]) + ".png"
-		image = row[1]
-		helpers.write_file(image,"cv\\board\\temp\\" + fileName)
-
-	#close the connection
+	#clean up, close connection, delete the image file
 	mydb.closeConnection()
+	os.remove(savePath)
 
-	#delete the original image file
-	helpers.delete_file(savePath)
+	
+
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from cv.board import annotate2,screenpls
+from cv.board import annotate, screenshot
+import argparse
 
-if __name__ == '__main__':
-    #annotate2.run()
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-a", "--annotate", action="store_true",
+					help="annotate unlabelled images")
+ap.add_argument("-s", "--screenshot", action="store_true",
+					help="take screenshot of main screen (pls screen chess game)")
+args = ap.parse_args()
 
-    screenpls.run()
+
+if args.annotate:
+	annotate.run()
+
+if args.screenshot:
+	screenshot.run()
