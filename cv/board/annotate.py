c62e354adfd4490deb0cc8143b4a462a1e71bd4d
Tyler Little <tyler10@umbc.edu>
2019-12-08 18:02:49 -0500

    big restructure: everything compartmentalizeseze

--- cv/board/annotate.py
+++ cv/board/annotate.py
@@ -11,130 +11,219 @@ from PIL import Image, ImageTk
 from s3 import S3_utils
 from sql import mysqlutil as db
 
-# UI class for annotating pieces
-class Piece(tk.Frame):
+class Annotate:
 
-    def __init__(self, master=None, img=None, color=None):
+	def __init__(self, img, window, color):
 
-        super().__init__(master)
+		# create UI container
+		root = tk.Tk()
 
-        self.master = master
+		# make image larger for user's eyes
+		piece = cv2.resize(img, None, fx=3, fy=3)
 
-        self.image = cv2.resize(img, None, fx=3, fy=3)
-        self.image = cv2.cvtColor(self.image, cv2.COLOR_GRAY2RGB)
-        self.image = Image.fromarray(self.image)
-        self.image = ImageTk.PhotoImage(self.image)
+		# get window
+		w = piece.shape[0] + 50 # get window width + 50 pixels for padding
+		h = piece.shape[1] + 50 # and height 
 
-        self.color = color
+		ws = root.winfo_screenwidth() # get width of the screen
+		hs = root.winfo_screenheight() # and height of the screen
 
-        self.pack()
-        self.create_widgets()
+		# calculate window placement
+		# new window
+		if (window == (0, 0)):
 
-    def create_widgets(self):
+			x = (ws/2) - (w/2)
+			y = (hs/2) - (h/2)
 
-        self.img = tk.Label(self, image=self.image)
-        self.img.pack(side="top")
+		# keep old window placement
+		else:
 
-        self.rook = tk.Button(self, text=" rook ", command=self.addRook)
-        self.rook.pack(side="left")
+			# align window to position from before
+			# i have no idea how to replace these values with constants
+			x = window[0] - 12
+			y = window[1] - 31
 
-        self.bishop = tk.Button(self, text=" bishop ", command=self.addBishop)
-        self.bishop.pack(side="left")
+		# set window placement
+		root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-        self.knight = tk.Button(self, text=" knight ", command=self.addKnight)
-        self.knight.pack(side="left")
 
-        self.king = tk.Button(self, text=" king ", command=self.addKing)
-        self.king.pack(side="left")
+		# create actual UI elements
+		root = Window(master=root, img=piece, color=color)
+	   
+		# run it
+		root.mainloop()
 
-        self.queen = tk.Button(self, text=" queen ", command=self.addQueen)
-        self.queen.pack(side="left")
+		self.window = root.window
+		self.label = root.label
 
-        self.pawn= tk.Button(self, text=" pawn ", command=self.addPawn)
-        self.pawn.pack(side="left")
+class Window(tk.Frame):
 
-    def say_hi(self):
-        print("hi there, everyone!")
+	# master: main window reference
+	# img: image of chess piece to be displayed
+	# color: color of chess piece (white/black)
+	# window: location of window relative to screen
+	def __init__(self, master=None, img=None, color=None, location=None):
 
-    def addRook(self):
+		super().__init__(master)
 
-    	self.label = "r"
-    	self.exit()
+		self.master = master
+		self.location = location
+		self.color = color
+		self.labels = { 
+						" rook ": "r", " bishop ": "b", " knight ": "n",
+						" king ": "k", " queen ": "q", " pawn ": "p" 
+						}
 
-    def addBishop(self):
+		# convert opencv image to PIL (Python Imaging Library?) format
+		self.image = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+		self.image = Image.fromarray(self.image)
+		self.image = ImageTk.PhotoImage(self.image)
 
-    	self.label = "b"
-    	self.exit()
+		self.pack()
+		self.create_widgets()
 
-    def addKnight(self):
+	# create UI elements
+	def create_widgets(self):
 
-    	self.label = "n"
-    	self.exit()
+		self.pieceImage = tk.Label(self, image=self.image).pack(side="top")
 
-    def addKing(self):
+		for x, y in self.labels.items():
+			button = tk.Button(self, text=x, command=lambda name=y: self.assignLabel(name))
+			button.pack(side="left")\
 
-    	self.label = "k"
-    	self.exit()
+	def assignLabel(self, label):
 
-    def addQueen(self):
+		self.label = label
+		self.exit()
 
-    	self.label = "q"
-    	self.exit()
+	def exit(self):
 
-    def addPawn(self):
+		if (self.color == "w"):
+			self.label = self.label.upper()
 
-    	self.label = "p"
-    	self.exit()
+		# save window for next annotation
+		self.window = (self.winfo_rootx(), self.winfo_rooty())
 
-    def exit(self):
-    	print("exit")
-    	if (self.color == "w"):
-    		self.label = self.label.upper()
+		self.master.destroy()
 
-    	labels.append(self.label)
+def labelPieces(pieces):
 
-    	window = (self.winfo_rootx(), self.winfo_rooty())
-    	print(window)
-    	self.master.destroy()
+	labels = []
 
-def getBoard(image, og, cropxy):
+	# keep location of window on screen to keep UI spawn consistent
+	window = (0, 0)
 
-    where = []
-    img = image.copy() 
-    start = round(img.shape[0] / 2)
-    beginning = 0
-    counter = 0
+	for piece in pieces:
 
-    # check for the two colors of the board and crop out excess
-    while (img[counter][start] < 100):
-        counter += 1
-    top = counter
+		image = piece.copy()
 
-    counter = beginning
-    while (img[start][counter] < 100):
-        counter += 1
-    left = counter
+		# get color
+		color = getColor(piece)
 
-    counter = img.shape[0] - 1
-    while (img[counter][start] < 100):
-        counter -= 1
-    bottom = counter
+		result = Annotate(image, window, color)
 
-    counter = img.shape[1] - 1
-    while (img[start][counter] < 100):
-        counter -= 1
-    right = counter
+		window = result.window
+		labels.append(result.label)
+		print(result.label)
 
-    where.append((cropxy[0][0] + left, cropxy[0][1] + top))
-    where.append((cropxy[1][0] - (img.shape[1] - right),
-                cropxy[1][1] - (img.shape[0] - bottom)))
+	return labels
 
-    board = img[top:bottom, left:right]
+def getColor(piece):
 
-    return board, where
+	piece = piece.copy()
+
+	middle = getMiddle(piece)
+
+	# filter color on average lightness
+	color = round(np.mean(middle))
+
+	if (color < 100):
+		color = "b"
+	else:
+		color = "w"
+
+	return color
+
+def getMiddle(piece):  
+	return piece[round(piece.shape[0] / 4):round(piece.shape[0] * (3/4)), 
+				round(piece.shape[1] / 4):round(piece.shape[1] * (3/4))]
+
+def getPieces(board):
+
+	x = 0
+	y = 0
+	pieces = []
+
+	board = board.copy()
+
+	unit = round(board.shape[0] / 8)
+
+	for k in range(8):
+
+		for l in range(8):
+
+			cell = board[unit * k:unit * (k+1), unit * l:unit * (l+1)]
+
+			# get middle of cell where piece will always be (if present)
+			middle = getMiddle(cell)
+
+			# make darker pixels white in this area to isolate pieces
+			middle = transform(middle)
+
+			# filter pieces on average lightness of a space
+			avg_color = round(np.mean(middle))
+
+			# found a piece
+			if (avg_color > 198):
+
+				pieces.append(cell)
+
+			x += unit
+
+		x = 0
+		y += unit
+
+	return pieces
+
+def getBoard(screenshot, og, cropxy):
+
+	where = []
+	img = screenshot.copy() 
+	start = round(img.shape[0] / 2)
+	beginning = 0
+	counter = 0
+
+	# check for the two colors of the board and crop out excess
+	while (img[counter][start] < 100):
+		counter += 1
+	top = counter
+
+	counter = beginning
+	while (img[start][counter] < 100):
+		counter += 1
+	left = counter
+
+	counter = img.shape[0] - 1
+	while (img[counter][start] < 100):
+		counter -= 1
+	bottom = counter
+
+	counter = img.shape[1] - 1
+	while (img[start][counter] < 100):
+		counter -= 1
+	right = counter
+
+	where.append((cropxy[0][0] + left, cropxy[0][1] + top))
+	where.append((cropxy[1][0] - (img.shape[1] - right),
+				cropxy[1][1] - (img.shape[0] - bottom)))
+
+	board = img[top:bottom, left:right]
+
+	return board, where
 
 
 def transform(image):
+
 	img = image.copy()
 
 	for y in range(image.shape[0]):
@@ -149,172 +238,86 @@ def transform(image):
 
 def run():
 
-    global __MAX_RETURN_ROWS__
-    global temp_folder_path
-    global root
-    global labels
-    global window
+	__MAX_RETURN_ROWS__ = 1
+	temp_folder_path = './cv/board/temp/'
+	root = '../..'
 
-    __MAX_RETURN_ROWS__ = 1
-    temp_folder_path = './cv/board/temp/'
-    root = '../..'
-    labels = []
-    window = (0, 0)
+	# query database for image paths that have not been annotated
+	mydb = db.dbConnection()
+	mydb.openConnection()
+	cursor = mydb.cursor
+	
+	# call procedure which returns filepaths, and IDs which are not currently locked and not already annotated
+	# also updates the locked flag before returning
+	cursor.callproc("chess.getBoardImages", [__MAX_RETURN_ROWS__, ])
+	results = cursor.stored_results()
 
-    # query database for image paths that have not been annotated
-    mydb = db.dbConnection()
-    mydb.openConnection()
-    cursor = mydb.cursor
-    
-    # call procedure which returns filepaths, and IDs which are not currently locked and not already annotated
-    # also updates the locked flag before returning
-    cursor.callproc("chess.getBoardImages", [__MAX_RETURN_ROWS__, ])
-    results = cursor.stored_results()
+	# parse eachresult set, and rows therein
+	for result in results:
 
-    # parse eachresult set, and rows therein
-    for result in results:
+		for row in result:
 
-        for row in result:
-            print(row)
-            # save the file, naming it with its unique ID
-            S3_utils.download_file('chessftw', row[1], temp_folder_path + str(row[0]) + '.png')
+			# save the file, naming it with its unique ID
+			S3_utils.download_file('chessftw', row[1], temp_folder_path + str(row[0]) + '.png')
 
 
-    # grab all files from ./cv/board/temp/
-    files = [temp_folder_path + f for f in os.listdir(temp_folder_path) if os.path.isfile(temp_folder_path + f)]
-    
-    # get all screenshots from temp dir
-    images = [i for i in files if (i[-4:] == ".png")]
+	# grab all files from ./cv/board/temp/
+	files = [temp_folder_path + f for f in os.listdir(temp_folder_path) if os.path.isfile(temp_folder_path + f)]
+	
+	# get all screenshots from temp dir
+	images = [i for i in files if (i[-4:] == ".png")]
 
-    images = images[:1]
+	images = images[:1]
+	for i in images:
 
-    for i in images:
-        print("now processing " + i)
+		print("now processing " + i)
 
-        img = cv2.imread(i, 0)
-        og = img.copy()
+		img = cv2.imread(i, 0)
+		og = img.copy()
 
-        # draw cropped image and dimensions of crop relative to original image
-        crop, cropxy = draw.draw(i)
+		# draw cropped image and dimensions of crop relative to original image
+		crop, cropxy = draw.draw(i)
 
-        # convert result to grayscale
-        img = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+		# convert result to grayscale
+		img = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
 
-        # crop out background pixels that are not the board
-        # also calculate starting x,y and ending x,y of board relative to original image
-        board, dimensions = getBoard(img, og, cropxy)
-        
+		# crop out background pixels that are not the board
+		# also calculate starting x,y and ending x,y of board relative to original image
+		board, dimensions = getBoard(img, og, cropxy)
 
-        # data to upload
-        id = i[:-4]
-        y1 = dimensions[1][0]
-        x1 = dimensions[0][0]
-        y2 = dimensions[1][1]
-        x2 = dimensions[1][0]
+		# data to upload
+		id = i[:-4]
+		y1 = dimensions[1][0]
+		x1 = dimensions[0][0]
+		y2 = dimensions[1][1]
+		x2 = dimensions[1][0]
 
-        # print("show crop from og image")
-        # test = og[dimensions[0][1]:dimensions[1][1], dimensions[0][0]:dimensions[1][0]]
-        # cv2.imshow("test", test)
-        # cv2.waitKey(0)
+		# print("show crop from og image")
+		# test = og[dimensions[0][1]:dimensions[1][1], dimensions[0][0]:dimensions[1][0]]
+		# cv2.imshow("test", test)
+		# cv2.waitKey(0)
 
-        # img = cv2.resize(img, None ,fx=0.25,fy=0.25)
-        # img = img[41:228, 143:329]
-        
-        # print(round(img.shape[0] / 8))
-        # print(img[0,0])
-        
-        x = 0
-        y = 0
-        numPieces = 0
+		pieces = getPieces(board)
 
-        pieces = []
-        piece_avg = []
-        
-        copy = board.copy()
-        unit = round(copy.shape[0] / 8)
+		print(f"found {len(pieces)} pieces")
 
+		labels = labelPieces(pieces)
 
-        for k in range(8):
+		files = []
 
-            for l in range(8):
+		for j in pieces:
 
-                piece = copy[unit * k:unit * (k+1), unit * l:unit * (l+1)]
-                middle = piece.copy()[round(piece.shape[0] / 4):round(piece.shape[0] * (3/4)), 
-                					round(piece.shape[1] / 4):round(piece.shape[1] * (3/4))]
+			file = str(uuid.uuid4()) + ".png"
 
-                # make darker pixels white to isolate pieces
-                # spaces with pieces on the board are now lighter than empty spaces
-                test = transform(middle)
+			files.append(file)
 
-                # filter on that
-                avg_color = round(np.mean(test))
-                piece_avg.append(avg_color)
-
-                if (avg_color > 198):
-
-                    # cv2.imshow("test", piece)
-                    # cv2.waitKey(0)
-
-                    file = str(uuid.uuid4()) + ".png"
-
-                    color = round(np.mean(middle))
-
-                    if (color < 100):
-                    	color = "b"
-                    else:
-                    	color = "w"
-
-                    numPieces += 1
-
-                    cv2.imwrite(file, piece)
+		print(files)
+		print(labels)
+			# cv2.imwrite(file, piece)
+			# pieces.append(file)
 
 
-                    # annotate pieces
-                    root = tk.Tk()
+			# window, label = LabelPiece(piece, window)
+			# # clean up 
+			# os.remove(file)
 
-                    w = 400 # width for the Tk root
-                    h = 250 # height for the Tk root
-
-                    ws = root.winfo_screenwidth() # width of the screen
-                    hs = root.winfo_screenheight() # height of the screen
-
-                    print(window)
-
-					# calculate x and y coordinates for the Tk root window
-                    if (window == (0, 0)):
-                    	x = (ws/2) - (w/2)
-                    	y = (hs/2) - (h/2)
-                    else:
-                    	x = window[0]
-                    	y = window[1]
-
-
-
-                    root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-                    root = Piece(master=root, img=piece, color=color)
-                    root.mainloop()
-
-                    pieces.append(file)
-
-                    print(pieces)
-                    print(labels)
-
-                    os.remove(file)
-
-                    # cv2.imshow("test", piece)
-                    # cv2.waitKey(0)
-
-                x += unit
-
-            x = 0
-            y += unit
-
-
-        # img = img[:unit, :unit]
-        
-        # img.show(plot)
-
-
-
-        # label = input("r k b q k p: ")
